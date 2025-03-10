834623e910c3a459d94335527ab1d26312737ebe
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2025-03-10 21:10:34 +0200

    unit

--- Sõne_ja_järjesti_funktsioonid.py
+++ Sõne_ja_järjesti_funktsioonid.py
@@ -1,4 +1,24 @@
 sõne = "Hello World"
 sõne_list=list(sõne)
+print(sõne_list)
 while True:
-    print(""
+    print("1. Сложение списков. ")
+    print("2. Повторение списка. ")
+    print("3. Длина списка. ")
+    print("4. Сборка строки из списка с разделителем S.")
+    valik=int(input( ))
+    if valik==1:
+        y_list=list(input("Введите любое слово "))
+        x=sõne_list + y_list
+        print(x)
+    elif valik==2:
+        kogus=int(input("Введите число"))
+        a=sõne_list * kogus
+        print(a)
+    elif valik==3:
+        b=len(sõne_list)
+        print(b)
+    elif valik==4:
+        s=" "
+        c=s.join(sõne_list)
+        print(c)
