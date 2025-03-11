aa0c559cd1e86732480235164c8be4614a70401a
Oleksandra Ryshniak <oleksandraryshniak@gmail.com>
2025-03-11 19:40:18 +0200

    unit

--- Sõne_ja_järjesti_funktsioonid.py
+++ Sõne_ja_järjesti_funktsioonid.py
@@ -2,21 +2,21 @@ sõne = "Hello World"
 sõne_list=list(sõne)
 print(sõne_list)
 while True:
-    print("1. Удалит индекс(индекс вводит пользователь). ")
-    print("2. Печатает все элементы в списке по очереди. ")
-    print("3. Переводит все буквы в списке к верхнему регистру. ")
-    print("4. Переводит все буквы в списке к нижнему регистру.")
-    print("5. Удаляем последний объект в списке. ")
-    print("6. Копирование списка.  ")
-    print("7. Расширяет список, добавляя в конец все элементы списка, которые введет пользователь. ")
-    print("8. Сложение списков.  ")
-    print("9. Вставляет значение, которое введет пользователь на позицию, которую также вводит пользователь. . ")
-    print("10. Очищение всего списка. ")
+    print("1. Kustutab indeksi (kasutaja sisestab indeksi). ")
+    print("2. Prindib kõik elemendid järjestikku loendis. ")
+    print("3. Teisendab kõik tähed loendis suurtähtedeks. ")
+    print("4. Teisendab kõik tähed loendis väiketähtedeks. ")
+    print("5. Kustutame viimase objekti loendist. ")
+    print("6. Loendi kopeerimine. ")
+    print("7. Laiendab loendit, lisades lõppu kõik elemendid, mille kasutaja sisestab. ")
+    print("8. Loendite liitmine. ")
+    print("9. Konkreetse elemendi muutmine loendis (kasutaja sisestab indeksi). ")
+    print("10. Kogu loendi tühjendamine. ")
     try:
         valik=int(input( ))
         if valik==1:
             try:
-                el=int(input("Введите число: "))
+                el=int(input("Sisesta number: "))
                 del sõne_list[el] 
                 print(sõne_list)
             except:
@@ -32,29 +32,49 @@ while True:
             print(c)
         elif valik==5:
             f=sõne_list.pop()
-            print(f"{f},результат - {sõne_list} ")
+            print(f"{f},tulemus - {sõne_list} ")
         elif valik==6:
             copy=sõne_list.copy()
             print(copy)
         elif valik==7:
-            l=input("Введите любое слово/предложение: ")
+            while 1:
+                try:
+                    l=input("Sisesta ükskõik milline sõna/lausung: ")
+                    if l.isalpha():break
+                except:
+                    print("Vale formaat!")
             l_list=list(l)
             d=sõne_list.extend(l)
             print(sõne_list)
         elif valik==8:
-           l_list=list(input("Введите любое значение: "))
-           summ=l_list+sõne_list
-           print(summ)
+            while 1:
+                try:
+                    k_list=list(input("Sisesta ükskõik milline väärtus: "))
+                    if k_list.isalpha(): break
+                except:
+                    print("Vale formaat!")
+            summ=k_list+sõne_list
+            print(summ)
         elif valik==9:
-            i=int(input("Введите число:"))
-            k=input("Введите любую букву/слово/предложение: ")
-            vastus=sõne_list.insert(i, k)
+            while True:
+                try:
+                    i=int(input("Sisesta number: "))
+                    break
+                except:
+                    print("Vale formaat!")
+            while 1:
+                try:
+                    k=input("Sisesta ükskõik milline täht/sõna/lausung: ")
+                    if k.isalpha(): break
+                except:
+                    print("Vale formaat!")
+            sõne_list[i]=k
             print(sõne_list)
         elif valik==10:
             res=sõne_list.clear()
             print(sõne_list)
         else:
-            print("Введите значение от 1 до 10. ")
+            print("Sisesta väärtus vahemikus 1 kuni 10. ")
     except:
         print("Value formaat! ")
      
